7d5d94f44632bf0d442906b4c6d455e0c1db1502
JonnyIvan24 <johnny.perez.u@gmail.com>
2023-02-25 10:20:40 -0600

    propagación de objetos y arreglos

---
+++ build/src/session-3/propagacion.js
@@ -0,0 +1,12 @@
+"use strict";
+let firstObj = { id: 1, name: 'firstObj' };
+let secondObj = Object.assign({}, firstObj);
+console.log('secondObjt : ' + JSON.stringify(secondObj));
+let nameObj = { name: 'nameObj' };
+let idObj = { id: 2 };
+let obj3 = Object.assign(Object.assign({}, nameObj), idObj);
+console.log(obj3);
+let firstArry = [1, 2, 3];
+let secondArry = [3, 4, 5];
+let thirdArry = [...firstArry, ...secondArry];
+console.log('thirdArry = ' + thirdArry);
