38bed53a22fb69a9dacdef3f25d70800f0126867
JonnyIvan24 <johnny.perez.u@gmail.com>
2023-03-04 10:50:17 -0600

    clases - implementando interfaces

--- src/session-4/clases.ts
+++ src/session-4/clases.ts
@@ -12,3 +12,41 @@ mySimpleClass.print()
 let mySimpleClassA = new SimpleClass()
 mySimpleClassA.id = 30
 mySimpleClassA.print()
+
+// implementación de interfaces
+class ClassA implements Print {
+    print() {
+        console.log('ClassA.print() ah sido llamado')
+    }
+}
+
+class ClassB implements Print {
+    id: number | undefined
+    print() {
+        console.log('ClassB.print() ah sido llamado')
+    }
+}
+
+interface Print {
+    print(): void;
+}
+
+function printClass(a: Print) {
+    a.print()
+}
+
+let myClassA = new ClassA()
+let myClassB = new ClassB()
+
+printClass(myClassA)
+printClass(myClassB)
+
+// tipado pato
+class ClassC {
+    print() {
+        console.log('ClassC.print() ah sido llamado')
+    }
+}
+
+let myClassC = new ClassC()
+printClass(myClassC)
