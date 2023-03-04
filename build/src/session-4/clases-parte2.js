62bb2a9869ee3754cd27c51ab3f1981b6f0009bc
JonnyIvan24 <johnny.perez.u@gmail.com>
2023-03-04 12:02:39 -0600

    Clases - private

--- build/src/session-4/clases-parte2.js
+++ build/src/session-4/clases-parte2.js
@@ -6,3 +6,22 @@ class ClassWithConstructor {
 }
 let classWC = new ClassWithConstructor(10);
 console.log(`classW = ${JSON.stringify(classWC)}`);
+class ClassWithPublicProperty {
+}
+let myClassWithPublicP = new ClassWithPublicProperty();
+myClassWithPublicP.id = 10;
+console.log(myClassWithPublicP.id);
+class ClassWithPrivateProperty {
+    constructor(id) {
+        this.id = id;
+    }
+    print() {
+        console.log(this.id);
+    }
+}
+let myClassWithPrivateP = new ClassWithPrivateProperty(10);
+// no podemos acceder a una propiedad privada
+// fuera de la clase
+// myClassWithPrivateP.id = 20
+// pero utilizar métodos públicos para acceder a ellos
+myClassWithPrivateP.print();
--- src/session-4/clases-parte2.ts
+++ src/session-4/clases-parte2.ts
@@ -8,3 +8,30 @@ class ClassWithConstructor {
 let classWC = new ClassWithConstructor(10)
 
 console.log(`classW = ${JSON.stringify(classWC)}`)
+
+class ClassWithPublicProperty {
+    public id: number | undefined
+}
+
+let myClassWithPublicP = new ClassWithPublicProperty()
+
+myClassWithPublicP.id = 10
+console.log(myClassWithPublicP.id)
+
+class ClassWithPrivateProperty {
+    private id: number
+    constructor (id: number) {
+        this.id = id
+    }
+
+    print() {
+        console.log(this.id)
+    }
+}
+
+let myClassWithPrivateP = new ClassWithPrivateProperty(10)
+// no podemos acceder a una propiedad privada
+// fuera de la clase
+// myClassWithPrivateP.id = 20
+// pero utilizar métodos públicos para acceder a ellos
+myClassWithPrivateP.print()
