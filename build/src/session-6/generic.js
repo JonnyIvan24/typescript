2b567985f3901eb677f6590187b01723aeb9d790
JonnyIvan24 <johnny.perez.u@gmail.com>
2023-03-18 11:51:58 -0600

    Retringiendo el tipado genérico

--- build/src/session-6/generic.js
+++ build/src/session-6/generic.js
@@ -10,3 +10,39 @@ printGeneric("test");
 printGeneric(true);
 printGeneric(() => { });
 printGeneric({ id: 1 });
+// Podemos definir n tipados genéricos, pero se recomienda
+// que se menor o igual a la cantidad de parametros que tenga la función
+function usingTwoTypes(first, second) { }
+// al espeficar el tipado, debemos señalar todos los tipos genéricos definidos
+usingTwoTypes(1, 2);
+usingTwoTypes(1, "test");
+usingTwoTypes("test", true);
+class Concatenetor {
+    concatenetArray(items) {
+        let returnString = "";
+        for (let i = 0; i < items.length; i++) {
+            returnString += i === 0 ? "" : ",";
+            returnString += items[i].toString();
+        }
+        return returnString;
+    }
+}
+let myConcatenetor = new Concatenetor();
+let concatResult = myConcatenetor.concatenetArray([
+    "first", "second", "third"
+]);
+console.log(concatResult);
+concatResult = myConcatenetor.concatenetArray([
+    1, 2, 3
+]);
+console.log(concatResult);
+// podemos reducir el tipado ya definido haciendo uso de
+// la definiciópn espécifica pero solo admite los tipos de valores que
+// indicamos en la definición de la clase
+let myConcatenetor2 = new Concatenetor();
+function useT(item) {
+    item.print();
+    // no lo permite por que la propiedad no es común en la interfaces
+    // item.id = 1
+    // item.name = "juan"
+}
